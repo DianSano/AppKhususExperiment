9fe9d3e1f39c808643474f0377d9db0a572ca961
DianSano <alb.vds@gmail.com>
2019-07-27 22:31:05 +0700

    Tambah activity untuk menampilkan ringkasan quiz

---
+++ app/src/main/java/id/co/blogspot/diansano/appkhususexperiment/RingkasanQuiz.java
@@ -0,0 +1,18 @@
+package id.co.blogspot.diansano.appkhususexperiment;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RingkasanQuiz extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ringkasan_quiz);
+
+        TextView ringkasanQuiz = findViewById(R.id.tv_ringkasan_quiz);
+        ringkasanQuiz.setText(getIntent().getStringExtra("ringkasanQuiz"));
+    }
+}
