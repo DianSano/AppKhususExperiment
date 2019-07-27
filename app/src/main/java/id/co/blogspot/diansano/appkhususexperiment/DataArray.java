f26870e85d1e1bb9322beef018b9860b2e266a8e
DianSano <alb.vds@gmail.com>
2019-07-28 00:21:37 +0700

    array dibuat maksimal 5 opsi tetapi opsi yang ditampilkan bisa kurang dari 5 (invisible)

--- app/src/main/java/id/co/blogspot/diansano/appkhususexperiment/DataArray.java
+++ app/src/main/java/id/co/blogspot/diansano/appkhususexperiment/DataArray.java
@@ -4,26 +4,29 @@ import java.util.ArrayList;
 
 public class DataArray {
 
-    String[][] q = {{"Pertanyaan 1", "A", "B", "C", "D"},
-                     {"Pertanyaan 2","A", "B", "C", "D"},
-                    {"Pertanyaan 3","A", "B", "C", "D"},
-                    };
+    String[][] q = {{"Pertanyaan 1", "A", "B", "C", "D", "E"},
+            {"Pertanyaan 2", "A", "B", "C", "D", "E"},
+            {"Pertanyaan 3", "A", "B", "C", "D", "E"},
+            {"Pertanyaan 4", "A", "B", "C", "D", "E"},
+            {"Pertanyaan 5", "A", "B", "C", "D", "E"}
+    };
 
     public ArrayList<Question> getQuestions() {
         ArrayList<Question> questionList = new ArrayList<>();
-        for (int i=0; i < q.length; i++) {
+        for (int i = 0; i < q.length; i++) {
 
+                Question question = new Question();
+                question.setQuestion(q[i][0]);
+                question.setOption1(q[i][1]);
+                question.setOption2(q[i][2]);
+                question.setOption3(q[i][3]);
+                question.setOption4(q[i][4]);
+                question.setOption5(q[i][5]);
+                //question.setAnswerNr(q[i][5]);
+                questionList.add(question);
 
-            Question question = new Question();
-            question.setQuestion(q[i][0]);
-            question.setOption1(q[i][1]);
-            question.setOption2(q[i][2]);
-            question.setOption3(q[i][3]);
-            question.setOption4(q[i][4]);
-            //question.setAnswerNr(q[i][5]);
-            questionList.add(question);
         }
-            return questionList;
+        return questionList;
 
     }
 }
