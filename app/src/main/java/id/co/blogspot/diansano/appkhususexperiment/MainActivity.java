d5ff9e405664849f479dfdb9b9d470d0705e8cf5
DianSano <alb.vds@gmail.com>
2019-07-27 22:57:41 +0700

    set radio button ke 5 INVISIBLE untuk pertanyaan tertentu

--- app/src/main/java/id/co/blogspot/diansano/appkhususexperiment/MainActivity.java
+++ app/src/main/java/id/co/blogspot/diansano/appkhususexperiment/MainActivity.java
@@ -21,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
     private RadioButton rb2;
     private RadioButton rb3;
     private RadioButton rb4;
+    private RadioButton rb5;
 
     private Button buttonConfirmNext;
 
@@ -28,13 +29,14 @@ public class MainActivity extends AppCompatActivity {
 
     private boolean answered;
 
-  //  private int score;
+    //  private int score;
 
     private int questionCounter;
     private int questionCountTotal;
     private Question currentQuestion;
 
     private String ringkasanQuiz = "";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,14 +49,12 @@ public class MainActivity extends AppCompatActivity {
         questionList = dataArray.getQuestions();
         questionCountTotal = questionList.size();
 
-     //   ringkasanQuiz = "Pertanyaan ke: " + (questionCounter + 1) + " ";
-
-
         rbGroup = findViewById(R.id.radio_group);
         rb1 = findViewById(R.id.radio_button1);
         rb2 = findViewById(R.id.radio_button2);
         rb3 = findViewById(R.id.radio_button3);
         rb4 = findViewById(R.id.radio_button4);
+        rb5 = findViewById(R.id.radio_button5);
         buttonConfirmNext = findViewById(R.id.button_confirm_next);
 
         showNextQuestion();
@@ -63,38 +63,34 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 if (!answered) {
-                    if (rb1.isChecked() || rb2.isChecked() || rb3.isChecked() || rb4.isChecked()) {
-                      //  checkAnswer();
+                    if (rb1.isChecked() || rb2.isChecked() || rb3.isChecked() || rb4.isChecked()
+                            || rb5.isChecked()) {
                         switch (rbGroup.getCheckedRadioButtonId()) {
-                            case R.id.radio_button1:
-                                //   rb1.setTextColor(Color.GREEN);
+                            case R.id.radio_button1:  //   rb1.setTextColor(Color.GREEN);
                                 tvSkor.setText("Skor: " + 0);
                                 ringkasanQuiz += "Anda pilih jawaban A skor: 0 \n";
-                             //   textViewQuestion.setText("Anda pilih jawaban A");
                                 break;
                             case R.id.radio_button2:
                                 tvSkor.setText("Skor: " + 1);
                                 ringkasanQuiz += "Anda pilih jawaban B skor: 1 \n";
-                                //     rb2.setTextColor(Color.GREEN);
-                              //  textViewQuestion.setText("Anda pilih jawaban B");
                                 break;
                             case R.id.radio_button3:
                                 tvSkor.setText("Skor: " + 2);
                                 ringkasanQuiz += "Anda pilih jawaban C skor: 2 \n";
-                                //   rb3.setTextColor(Color.GREEN);
-                              //  textViewQuestion.setText("Anda pilih Jawaban C");
                                 break;
                             case R.id.radio_button4:
                                 tvSkor.setText("Skor: " + 3);
                                 ringkasanQuiz += "Anda pilih jawaban D skor: 3 \n";
-                                //   rb3.setTextColor(Color.GREEN);
-                             //   textViewQuestion.setText("Anda pilih jawaban D");
+                                break;
+                            case R.id.radio_button5:
+                                tvSkor.setText("Skor: " + 4);
+                                ringkasanQuiz += "Anda pilih jawaban E skor: 4 \n";
                                 break;
                         }
 
                         showNextQuestion();
-                        } else {
-                        Toast.makeText(MainActivity.this, "Please select an answer",
+                    } else {
+                        Toast.makeText(MainActivity.this, "Pilih salah satu jawaban",
                                 Toast.LENGTH_SHORT).show();
                     }
                 } else {
@@ -105,6 +101,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void showNextQuestion() {
+        //pertanyaan ke-2 dan ke-4 ada 5 radio button yang lain hanya 4
+        if (questionCounter == 1 || questionCounter == 3) {
+            rb5.setVisibility(View.VISIBLE);
+        } else {
+            rb5.setVisibility(View.INVISIBLE);
+        }
         if (questionCounter < questionCountTotal) {
             rbGroup.clearCheck();
             currentQuestion = questionList.get(questionCounter);
@@ -114,6 +116,7 @@ public class MainActivity extends AppCompatActivity {
             rb2.setText(currentQuestion.getOption2());
             rb3.setText(currentQuestion.getOption3());
             rb4.setText(currentQuestion.getOption4());
+            rb5.setText(currentQuestion.getOption5());
 
             ringkasanQuiz += "Pertanyaan ke: " + (questionCounter + 1) + " ";
             questionCounter++;
@@ -123,15 +126,16 @@ public class MainActivity extends AppCompatActivity {
 
 
         } else {
-            finishQuiz();
             // pindah ke intent ......
+            finishQuiz();
+
         }
     }
 
     private void finishQuiz() {
         Intent intent = new Intent(this, RingkasanQuiz.class);
         intent.putExtra("ringkasanQuiz", ringkasanQuiz);
-                startActivity(intent);
+        startActivity(intent);
     }
 
 }
